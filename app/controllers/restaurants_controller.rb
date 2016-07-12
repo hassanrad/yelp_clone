8e64b9c9ad7dde3d2d9225bd1b945a6698f58c38
hassanrad <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-07-12 12:35:25 +0100

    Add Edit and Update Routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,10 +13,24 @@ class RestaurantsController < ApplicationController
     redirect_to '/restaurants'
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 
 end
